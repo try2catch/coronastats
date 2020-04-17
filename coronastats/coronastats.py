780a02384686cbb75073dc80ebcb11299990fe60
akhileshsingh <trycatch55@gmail.com>
2020-04-17 13:45:43 +0530

    unable to locate element: {"method":"xpath","selector":"//tr[33]/te[13]"} : RESOLVED

--- coronastats/coronastats.py
+++ coronastats/coronastats.py
@@ -3,53 +3,41 @@ import sys
 from selenium import webdriver
 
 
-class CoronaStats:
+class CoronaStats():
     def __init__(self, country, website):
-        self.driver = webdriver.Chrome('<Driver Path>')
+        self.driver = webdriver.Chrome('<DRIVER PATH>')
         self.country = country
         self.website = website
 
-    def get_row_count(self, table):
-        return len(table.find_elements_by_tag_name('tr')) - 1
-
-    def get_columns_count(self, table):
-        return len(table.find_elements_by_xpath('//tr[2]/td'))
-
-    def check_none(self, value):
+    # If not data available for an attribute then return 0 else int value
+    @staticmethod
+    def check_none(value):
         if value == '':
-            return 0
+            return '0'
         else:
-            return int(value)
+            return value
 
+    # Reading data where it found the country name
     def scrapping_data(self, table):
+        # Get number of rows
+        country_element = table.find_element_by_xpath("//td[contains(., '{}')]".format(self.country))
+        row = country_element.find_element_by_xpath("./..")
+        data = row.text.split(" ")
 
-        noOfRows = self.get_row_count(table)
-        noOfColumns = self.get_columns_count(table)
-
-        allData = []
-        for i in range(1, noOfRows):
-            found_country = table.find_element_by_xpath("//tr[" + str(i) + "]/td[1]").text
-
-            if found_country == self.country:
-                for j in range(1, noOfColumns):
-                    allData.append(table.find_element_by_xpath("//tr[" + str(i) + "]/td[" + str(j) + "]").text)
-
-                break
-        return allData
+        return data
 
     def get_data(self):
         try:
             self.driver.get(self.website)
-            table = self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/tbody[1]')
+            table = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
             data = self.scrapping_data(table)
 
-            total_cases = self.check_none(data[1])
-            new_cases = self.check_none(data[2])
-            total_deaths = self.check_none(data[3])
-            new_deaths = self.check_none(data[4])
-            total_recovered = self.check_none(data[5])
-            active_cases = self.check_none(data[6])
-            serious_critical = self.check_none(data[7])
+            total_cases = CoronaStats.check_none(data[1])
+            new_cases = CoronaStats.check_none(data[2])
+            total_deaths = CoronaStats.check_none(data[3])
+            total_recovered = CoronaStats.check_none(data[5])
+            active_cases = CoronaStats.check_none(data[6])
+            serious_critical = CoronaStats.check_none(data[7])
 
             self.driver.close()
             return total_cases, new_cases, total_deaths, active_cases, total_recovered, serious_critical
